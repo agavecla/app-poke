f48fa64f79b25a1dccc7f06d2b396da568d4d6c6
Claudio Aragão de Matos <aramatos@gmail.com>
2020-02-23 19:36:52 -0300

    aa

--- server.js
+++ server.js
@@ -5,14 +5,14 @@ const path = require('path');
 const app = express();
 
 // Serve os arquivos estáticos da pasta dist (gerada pelo ng build)
-app.use(express.static(__dirname + '/dist/'));
+app.use(express.static(__dirname + '/dist/poke/'));
 
 // Inicia a aplicação pela porta configurada
 app.listen(process.env.PORT || 8080);
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/index.html'));
+res.sendFile(path.join(__dirname+'/dist/poke/index.html'));
 });
 
 console.log('console ouvindo');

663ee9c19ee6b8ec3f66c2ddfc5fe2c72556e219
Claudio Aragão de Matos <aramatos@gmail.com>
2020-02-23 19:22:22 -0300

    aa

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const path = require('path');
 const app = express();
 
 // Serve os arquivos estáticos da pasta dist (gerada pelo ng build)
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static(__dirname + '/dist/'));
 
 // Inicia a aplicação pela porta configurada
 app.listen(process.env.PORT || 8080);
